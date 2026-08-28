7db65c28f0042a6a04c387b2d5bd975edd429563
Solo600-glitch <m-12222398@moe-dl.edu.my>
2026-08-28 10:52:11 +0800

    RemovedFirst2ndLine.py

--- app.py
+++ app.py
@@ -263,9 +263,6 @@ G28 ; Home all axes
 M302 S0 ; Allow cold extrusion
 G1 Z2.2 F3000
 G1 X0.1 Y20 Z0.3 F5000.0 ; Move to start position
-G1 X0.1 Y200.0 Z0.3 F1500.0 E15 ; Draw the first line
-G1 X0.4 Y200.0 Z0.3 F5000.0 ; Move to side a little
-G1 X0.4 Y20 Z0.3 F1500.0 E30 ; Draw the second line
 G92 E0 ; Reset Extruder
 G1 Z2.2 F3000
 G1 X5 Y20 Z0.3 F5000.0 ; Move over to prevent blob
