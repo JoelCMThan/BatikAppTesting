07f92e3d13cde3c28798c37f42e0444e859631ac
Solo600-glitch <m-12222398@moe-dl.edu.my>
2026-08-28 14:18:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -261,10 +261,10 @@ M109 S80
 G92 E0 ; Reset Extruder
 G28 ; Home all axes
 M302 S0 ; Allow cold extrusion
-G1 Z2.2 F3000
+G1 Z8 F3000
 G1 X0.1 Y20 Z0.3 F5000.0 ; Move to start position
 G92 E0 ; Reset Extruder
-G1 Z2.2 F3000
+G1 Z8 F3000
 G1 X5 Y20 Z0.3 F5000.0 ; Move over to prevent blob
 G92 E0
 """.strip()
